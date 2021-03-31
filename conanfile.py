095e12f1eda31126ffb52fc512098acd17fb0b75
yengsu <yengsu@yengsu.com>
2021-03-31 11:53:05 +0800

    增加编译选项

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class ZLMediaKitConan(ConanFile):
     description = "A lightweight RTSP/RTMP/HTTP/HLS/HTTP-FLV/WebSocket-FLV/HTTP-TS/HTTP-fMP4/WebSocket-TS/WebSocket-fMP4/GB28181 server and client framework based on C++11"
     topics = ("http", "rtsp", "mp4", "hls", "rtmp", "websocket", "flv", "ts", "http-flv", "gb28181", "websocket-flv", "http-ts", "http-fmp4", "websocket-fmp4", "websocket-ts")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [True, False], "hls": [True, False], "openssl": [True, False], "mysql": [True, False], "faac": [True, False], "x264": [True, False], "mp4": [True, False], "rtpproxy": [True, False], "c_api":[True, False], "cxx_api": [True, False], "mem_debug": [True, False], "asan": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "hls": True, "openssl": True, "mysql": True, "faac": False, "x264": True, "mp4": True, "rtpproxy": True, "c_api": True, "cxx_api": True, "mem_debug": False, "asan": True}
     generators = "cmake", "cmake_find_package", "cmake_paths"
-    requires = ["ZLToolKit/4.0", "libx264/20191217"]
+    requires = ["ZLToolKit/4.0", "libx264/20191217", "openssl/1.1.1i", "libmysqlclient/8.0.17", "jemalloc/5.2.1", "libx264/20191217"]
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -41,9 +41,9 @@ set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})''')
         tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "set(LINK_LIB_LIST zlmediakit zltoolkit)", '''set(LINK_LIB_LIST zlmediakit ${CONAN_LIBS})''')
         tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "set_target_properties(zltoolkit PROPERTIES COMPILE_FLAGS ${VS_FALGS} )", '''#set_target_properties(zltoolkit PROPERTIES COMPILE_FLAGS ${VS_FALGS} )''')
         tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "set_target_properties(zlmediakit PROPERTIES COMPILE_FLAGS ${VS_FALGS} )", '''#set_target_properties(zlmediakit PROPERTIES COMPILE_FLAGS ${VS_FALGS} )''')
-        tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(api)", '''#add_subdirectory(api)''')
-        tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(tests)", '''#add_subdirectory(tests)''')
-        tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(server)", '''#add_subdirectory(server)''')
+        # tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(api)", '''#add_subdirectory(api)''')
+        # tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(tests)", '''#add_subdirectory(tests)''')
+        # tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_subdirectory(server)", '''#add_subdirectory(server)''')
 
         if self.options.shared:
         	tools.replace_in_file("ZLMediaKit/CMakeLists.txt", "add_library(zlmediakit STATIC ${MediaKit_src_list})", ''' add_library(ZLMediaKit SHARED ${MediaKit_src_list}) ''')
@@ -53,6 +53,75 @@ set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})''')
 
     def build(self):
         cmake = CMake(self)
+        if self.options.hls:
+            cmake.definitions["ENABLE_HLS"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_HLS"] = "False"
+            pass
+        if self.options.openssl:
+            cmake.definitions["ENABLE_OPENSSL"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_OPENSSL"] = "false"
+            pass
+        if self.options.mysql:
+            cmake.definitions["ENABLE_MYSQL"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_MYSQL"] = "false"
+            pass
+        if self.options.faac:
+            cmake.definitions["ENABLE_FAAC"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_FAAC"] = "false"
+            pass
+        if self.options.x264:
+            cmake.definitions["ENABLE_X264"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_X264"] = "false"
+            pass
+        if self.options.mp4:
+            cmake.definitions["ENABLE_MP4"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_MP4"] = "false"
+            pass
+        if self.options.rtpproxy:
+            cmake.definitions["ENABLE_RTPPROXY"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_RTPPROXY"] = "false"
+            pass
+        if self.options.c_api:
+            cmake.definitions["ENABLE_API"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_API"] = "false"
+            pass
+        if self.options.cxx_api:
+            cmake.definitions["ENABLE_CXX_API"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_CXX_API"] = "false"
+            pass
+        if self.options.mem_debug:
+            cmake.definitions["ENABLE_MEM_DEBUG"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_MEM_DEBUG"] = "false"
+            pass
+        if self.options.asan:
+            cmake.definitions["ENABLE_ASAN"] = "true"
+            pass
+        else:
+            cmake.definitions["ENABLE_ASAN"] = "false"
+            pass
+            
+        cmake.definitions["ENABLE_SERVER"] = "false" # 关闭服务器模块
+        cmake.definitions["ENABLE_TESTS"] = "false" # 关闭测试模块
         cmake.configure(source_folder="ZLMediaKit")
         cmake.build()
 
